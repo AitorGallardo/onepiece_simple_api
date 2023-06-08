308e07f0322bdc9eca84830d6cb5ff8ca67af09f
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-06-08 22:30:16 +0200

    auth apikey

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const data = require('./onepiece_eastblue.json'); // Replace 'data.json' with the actual filename and path to your JSON file
 
+require('dotenv').config();
+
+const apiKeyMiddleware = require('./middlewares');
 
 const app = express();
 
+app.use(apiKeyMiddleware);
 
 app.get('/api/data', (req, res) => {
   res.json(data);
